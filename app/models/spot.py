6d1ed3c298067ac5fae0d16c38e47cf3831c161b
kevin <kevinzy17@gmail.com>
2022-05-27 09:20:39 -0700

    migrations

--- app/models/spot.py
+++ app/models/spot.py
@@ -19,7 +19,7 @@ class Spot(db.Model):
 
     # many to many: user hasmany spots <--> spots has many users
     # tables: user and spots, secondary = wishlist
-    spot_wishlist = db.relationship("User", secondary="wishlists", back_populates="user_wishlist")
+    # spot_wishlist = db.relationship("User", secondary="wishlists", back_populates="user_wishlist")
 
     # many to one: spot belongsTo user (line 8 + this below connects)
     user = db.relationship("User", back_populates="spot")
