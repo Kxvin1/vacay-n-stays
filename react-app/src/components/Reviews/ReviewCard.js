eb9bae2a772e47a966e8384a8eecf02597ca7600
midknightsurfer <beau@hirethedeveloper.com>
2022-05-27 12:16:28 -0500

    Beau - 5/27/22 - Reviews Frontend

---
+++ react-app/src/components/Reviews/ReviewCard.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import ReviewCard from "./ReviewCard";
+import ReviewForm from "./ReviewForm";
+
+const Reviews = ({ user, spot, reviews }) => {
+  return <></>;
+};
+
+export default Reviews;
