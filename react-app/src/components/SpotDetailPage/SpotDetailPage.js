af52391b6e850eb54a3a6d8ef4223daa2976c76d
joshua <jrbauti1@asu.edu>
2022-05-28 09:35:09 -0700

    added modal to delete spot

---
+++ react-app/src/components/SpotDetailPage/SpotDetailPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./SpotDetailPage.css";
+
+export default function SpotDetailPage() {
+  return (
+    <div className="spot_detail_main_container">
+      <div className="spot_image_container"></div>
+      <div className="spot_detail_container"></div>
+    </div>
+  );
+}
