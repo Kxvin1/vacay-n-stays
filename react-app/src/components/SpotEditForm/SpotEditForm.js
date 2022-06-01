bfd3a5b857de45f07101b276216fa7e27cd6d9e6
joshua <jrbauti1@asu.edu>
2022-06-01 11:39:15 -0700

    posting a spot redirecting to detail page

--- react-app/src/components/SpotEditForm/SpotEditForm.js
+++ react-app/src/components/SpotEditForm/SpotEditForm.js
@@ -70,6 +70,7 @@ export default function SpotEditForm() {
     const spotData = await dispatch(editSpot(data, spotToEdit.id));
 
     await addImages(images, spotData[1].id);
+    history.push(`/spots/${spotData[1].id}`);
   };
 
   const addImages = async (images, spot_id) => {
@@ -102,105 +103,109 @@ export default function SpotEditForm() {
       <div className="spot_form_content">
         {" "}
         <form className="spot_form" onSubmit={handleSubmit}>
-          <div className="spot_name_container_input">
-            <label>Name:</label>
-            <input
-              name="name"
-              type="input"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            ></input>
-          </div>
-          <div className="spot_description_input">
-            <label>Description:</label>
-            <textarea
-              name="description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              required
-            ></textarea>
-          </div>
-          <div className="spot_address_input">
-            <label>Address:</label>
-            <Map
-              setCoordinates={setCoordinates}
-              setAddress={setAddress}
-              coordinates={coordinates}
-              address={address}
-            />
-          </div>
-          <div className="spot_city_input">
-            <label>City:</label>
-            <input
-              name="city"
-              type="text"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-              required
-            ></input>
-          </div>
-          <div className="spot_state_input">
-            <label>State:</label>
-            <select
-              value={state}
-              onChange={(e) => setState(e.target.value)}
-              className="spot_state_select"
-            >
-              <option value="AL">Alabama</option>
-              <option value="AK">Alaska</option>
-              <option value="AZ">Arizona</option>
-              <option value="AR">Arkansas</option>
-              <option value="CA">California</option>
-              <option value="CO">Colorado</option>
-              <option value="CT">Connecticut</option>
-              <option value="DE">Delaware</option>
-              <option value="DC">District Of Columbia</option>
-              <option value="FL">Florida</option>
-              <option value="GA">Georgia</option>
-              <option value="HI">Hawaii</option>
-              <option value="ID">Idaho</option>
-              <option value="IL">Illinois</option>
-              <option value="IN">Indiana</option>
-              <option value="IA">Iowa</option>
-              <option value="KS">Kansas</option>
-              <option value="KY">Kentucky</option>
-              <option value="LA">Louisiana</option>
-              <option value="ME">Maine</option>
-              <option value="MD">Maryland</option>
-              <option value="MA">Massachusetts</option>
-              <option value="MI">Michigan</option>
-              <option value="MN">Minnesota</option>
-              <option value="MS">Mississippi</option>
-              <option value="MO">Missouri</option>
-              <option value="MT">Montana</option>
-              <option value="NE">Nebraska</option>
-              <option value="NV">Nevada</option>
-              <option value="NH">New Hampshire</option>
-              <option value="NJ">New Jersey</option>
-              <option value="NM">New Mexico</option>
-              <option value="NY">New York</option>
-              <option value="NC">North Carolina</option>
-              <option value="ND">North Dakota</option>
-              <option value="OH">Ohio</option>
-              <option value="OK">Oklahoma</option>
-              <option value="OR">Oregon</option>
-              <option value="PA">Pennsylvania</option>
-              <option value="RI">Rhode Island</option>
-              <option value="SC">South Carolina</option>
-              <option value="SD">South Dakota</option>
-              <option value="TN">Tennessee</option>
-              <option value="TX">Texas</option>
-              <option value="UT">Utah</option>
-              <option value="VT">Vermont</option>
-              <option value="VA">Virginia</option>
-              <option value="WA">Washington</option>
-              <option value="WV">West Virginia</option>
-              <option value="WI">Wisconsin</option>
-              <option value="WY">Wyoming</option>
-            </select>
-          </div>
-          {/* <div className="spot_country_input">
+          <div className="spot_form_input_container">
+            <div className="spot_form_field_container">
+              <div className="spot_input_main_container">
+                <h3>Edit your Spot:</h3>
+                <div className="spot_name_container_input spot_input">
+                  <label className="label">Name:</label>
+                  <input
+                    name="name"
+                    type="input"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  ></input>
+                </div>
+                <div className="spot_description_input spot_input">
+                  <label className="label">Description:</label>
+                  <textarea
+                    name="description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    required
+                  ></textarea>
+                </div>
+                <div className="spot_address_input spot_input">
+                  <label className="label">Address:</label>
+                  <Map
+                    setCoordinates={setCoordinates}
+                    setAddress={setAddress}
+                    coordinates={coordinates}
+                    address={address}
+                  />
+                </div>
+                <div className="spot_city_input spot_input">
+                  <label className="label">City:</label>
+                  <input
+                    name="city"
+                    type="text"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    required
+                  ></input>
+                </div>
+                <div className="spot_state_input spot_input">
+                  <label className="label">State:</label>
+                  <select
+                    value={state}
+                    onChange={(e) => setState(e.target.value)}
+                    className="spot_state_select"
+                  >
+                    <option value="AL">Alabama</option>
+                    <option value="AK">Alaska</option>
+                    <option value="AZ">Arizona</option>
+                    <option value="AR">Arkansas</option>
+                    <option value="CA">California</option>
+                    <option value="CO">Colorado</option>
+                    <option value="CT">Connecticut</option>
+                    <option value="DE">Delaware</option>
+                    <option value="DC">District Of Columbia</option>
+                    <option value="FL">Florida</option>
+                    <option value="GA">Georgia</option>
+                    <option value="HI">Hawaii</option>
+                    <option value="ID">Idaho</option>
+                    <option value="IL">Illinois</option>
+                    <option value="IN">Indiana</option>
+                    <option value="IA">Iowa</option>
+                    <option value="KS">Kansas</option>
+                    <option value="KY">Kentucky</option>
+                    <option value="LA">Louisiana</option>
+                    <option value="ME">Maine</option>
+                    <option value="MD">Maryland</option>
+                    <option value="MA">Massachusetts</option>
+                    <option value="MI">Michigan</option>
+                    <option value="MN">Minnesota</option>
+                    <option value="MS">Mississippi</option>
+                    <option value="MO">Missouri</option>
+                    <option value="MT">Montana</option>
+                    <option value="NE">Nebraska</option>
+                    <option value="NV">Nevada</option>
+                    <option value="NH">New Hampshire</option>
+                    <option value="NJ">New Jersey</option>
+                    <option value="NM">New Mexico</option>
+                    <option value="NY">New York</option>
+                    <option value="NC">North Carolina</option>
+                    <option value="ND">North Dakota</option>
+                    <option value="OH">Ohio</option>
+                    <option value="OK">Oklahoma</option>
+                    <option value="OR">Oregon</option>
+                    <option value="PA">Pennsylvania</option>
+                    <option value="RI">Rhode Island</option>
+                    <option value="SC">South Carolina</option>
+                    <option value="SD">South Dakota</option>
+                    <option value="TN">Tennessee</option>
+                    <option value="TX">Texas</option>
+                    <option value="UT">Utah</option>
+                    <option value="VT">Vermont</option>
+                    <option value="VA">Virginia</option>
+                    <option value="WA">Washington</option>
+                    <option value="WV">West Virginia</option>
+                    <option value="WI">Wisconsin</option>
+                    <option value="WY">Wyoming</option>
+                  </select>
+                </div>
+                {/* <div className="spot_country_input">
         <label>Country:</label>
         <input
           name="country"
@@ -209,77 +214,86 @@ export default function SpotEditForm() {
           onChange={(e) => setCountry(e.target.value)}
         ></input>
       </div> */}
-          <div className="spot_price_input">
-            <label>Price $:</label>
-            <input
-              class="spot_price"
-              type="number"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-            ></input>
-          </div>
-          <div className="formInputSection" id="imageUploadSection">
-            <div className="fieldSection">
-              <h3 className="imagesHeader">Images</h3>
-              <div className="imageUploadContainer">
-                <ImageUploading
-                  multiple
-                  value={images}
-                  onChange={(imageList) => setImages(imageList)}
-                  maxNumber={80}
-                  dataURLKey="data_url"
-                  acceptType={["jpg", "png", "jpeg"]}
-                >
-                  {({
-                    imageList,
-                    onImageUpload,
-                    onImageRemoveAll,
-                    onImageUpdate,
-                    onImageRemove,
-                    isDragging,
-                    dragProps,
-                  }) => (
-                    <div className="upload__image-wrapper">
-                      <div
-                        style={
-                          isDragging ? { color: "rgb(192, 53, 22)" } : undefined
-                        }
-                        onClick={onImageUpload}
-                        {...dragProps}
-                        className="clickDragHere"
+                <div className="spot_price_input spot_input">
+                  <label className="label">Price $:</label>
+                  <input
+                    class="spot_price"
+                    type="number"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                  ></input>
+                </div>
+                <div className="formInputSection" id="imageUploadSection">
+                  <div className="field_section_container">
+                    <h3 className="imagesHeader">Images</h3>
+                    <div className="imageUploadContainer">
+                      <ImageUploading
+                        multiple
+                        value={images}
+                        onChange={(imageList) => setImages(imageList)}
+                        maxNumber={80}
+                        dataURLKey="data_url"
+                        acceptType={["jpg", "png", "jpeg"]}
                       >
-                        Add or Drag Images Here
-                      </div>
-                      {/* <div onClick={onImageRemoveAll}>Remove all images</div> */}
-                      <div className="uploadedImagesContainer">
-                        {imageList.map((image, index) => (
-                          <div key={index}>
-                            <img src={image["data_url"]} alt="" height="230" />
-                            <div className="editPhotoButtons">
-                              <div
-                                className="updatePhoto"
-                                onClick={() => onImageUpdate(index)}
-                              >
-                                Update
-                              </div>
-                              <div
-                                className="updatePhoto removePhoto"
-                                onClick={() => onImageRemove(index)}
-                              >
-                                Remove
-                              </div>
+                        {({
+                          imageList,
+                          onImageUpload,
+                          onImageRemoveAll,
+                          onImageUpdate,
+                          onImageRemove,
+                          isDragging,
+                          dragProps,
+                        }) => (
+                          <div className="upload__image-wrapper">
+                            <div
+                              style={
+                                isDragging
+                                  ? { color: "rgb(192, 53, 22)" }
+                                  : undefined
+                              }
+                              onClick={onImageUpload}
+                              {...dragProps}
+                              className="add_images_container"
+                            >
+                              Add or Drag Images Here
+                            </div>
+                            {/* <div onClick={onImageRemoveAll}>Remove all images</div> */}
+                            <div className="images_container">
+                              {imageList.map((image, index) => (
+                                <div key={index}>
+                                  <img
+                                    src={image["data_url"]}
+                                    alt=""
+                                    height="230"
+                                  />
+                                  <div className="editPhotoButtons">
+                                    <div
+                                      className="updatePhoto"
+                                      onClick={() => onImageUpdate(index)}
+                                    >
+                                      Update
+                                    </div>
+                                    <div
+                                      className="updatePhoto removePhoto"
+                                      onClick={() => onImageRemove(index)}
+                                    >
+                                      Remove
+                                    </div>
+                                  </div>
+                                </div>
+                              ))}
                             </div>
                           </div>
-                        ))}
-                      </div>
+                        )}
+                      </ImageUploading>
                     </div>
-                  )}
-                </ImageUploading>
+                  </div>
+                </div>
+                <div className="spot_button_div">
+                  <button type="submit">Submit</button>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="spot_button_div">
-            <button type="submit">Submit</button>
           </div>
         </form>
       </div>
